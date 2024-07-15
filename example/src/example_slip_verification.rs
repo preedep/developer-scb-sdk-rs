d416cdc066268262672cbda65495c81d3ac86073
Preedee Ponchevin <preedee.digital@gmail.com>
2024-07-15 17:21:53 +0700

    update code for bill payment inquiry

--- example/src/example_slip_verification.rs
+++ example/src/example_slip_verification.rs
@@ -1,4 +1,5 @@
 use log::info;
+use corescbsdk::entities::bill_pay::BillPaymentInquiryRequest;
 use corescbsdk::frameworks::apis::scb::SCBClientAPI;
 
 #[tokio::main]
@@ -13,8 +14,32 @@ async fn main(){
 
 
     let mut scb_client = SCBClientAPI::new(&application_name, &application_key, &secret_key);
-    let r = scb_client.get_slip_verification_qr30(&"12345".to_string(),
-    &"014".to_string()).await;
+    let r = scb_client.get_slip_verification_qr30(
+        &"12345".to_string(),
+        &"014".to_string()
+    ).await;
+
+    match r {
+        Ok(res) => {
+            info!("Response: {:#?}", res);
+        }
+        Err(e) => {
+            info!("Error: {:#?}", e);
+        }
+    }
+
+
+    let r = scb_client.query_bill_payment_transaction(
+        &BillPaymentInquiryRequest {
+            event_code: "00300100".to_string(),
+            transaction_date: "2019-08-22".to_string(),
+            biller_id: None,
+            reference1: None,
+            reference2: None,
+            partner_transaction_id: None,
+            amount: None,
+        }
+    ).await;
 
     match r {
         Ok(res) => {
