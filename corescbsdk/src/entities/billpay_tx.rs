4d23a0f1717cd88f91df4452af8ba62cb8b2d7c9
Preedee Ponchevin <preedee.digital@gmail.com>
2024-07-15 13:45:56 +0700

    add bill payment transaction

--- corescbsdk/src/entities/billpay_tx.rs
+++ corescbsdk/src/entities/billpay_tx.rs
@@ -2,19 +2,47 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct BillPaymentTransaction {
+    // Transaction Slip ID
+    #[serde(rename = "transRef")]
     pub trans_ref: String,
+    // Sender Bank Code
+    #[serde(rename = "sendingBank")]
     pub sending_bank: String,
+    // Receiving Bank Code
+    #[serde(rename = "receivingBank")]
     pub receiving_bank: String,
+    // Transaction Date
+    #[serde(rename = "transDate")]
     pub trans_date: String,
+    // Transaction Time
+    #[serde(rename = "transTime")]
     pub trans_time: String,
+    // See Sender
+    #[serde(rename = "sender")]
     pub sender: Sender,
+    // See Receiver
+    #[serde(rename = "receiver")]
     pub receiver: Receiver,
+    // Transaction Amount
+    #[serde(rename = "amount")]
     pub amount: String,
+    // Local Amount
+    #[serde(rename = "paidLocalAmount")]
     pub paid_local_amount: String,
+    // Local Currency
+    #[serde(rename = "paidLocalCurrency")]
     pub paid_local_currency: String,
+    // Country Code
+    #[serde(rename = "countryCode")]
     pub country_code: String,
+    // Reference 1
+    #[serde(rename = "ref1")]
     pub ref1: String,
+    // Reference 2
+    #[serde(rename = "ref2")]
     pub ref2: String,
+    // Reference 3
+    #[serde(rename = "ref3")]
     pub ref3: String,
 }
 
